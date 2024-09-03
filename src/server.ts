fd7e6223f73a913d1e3bb38fc7e41a23eb46c525
Sergio <spn1994@hotmail.com>
2024-09-03 14:18:02 -0300

    Msg de erro no servidor

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
 import fastify from 'fastify';
 
-const app = fastify()
+const app = fastify();
 
-app.get('/hello', () => {
-  return 'Hello Word'
-})
+app.get('/hello', (request, reply) => {
+  return 'Hello World';
+});
 
 app.listen({
   port: 3333
-}).then (() => {
-  console.log('Server is running')
-})
+}).then(() => {
+  console.log('Server is running on http://localhost:3333');
+}).catch(err => {
+  console.error('Erro no servidor', err);
+  process.exit(1);
+});
